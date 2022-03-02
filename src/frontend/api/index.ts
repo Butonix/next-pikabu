269776cd2899e83f9243061990e027037fec3274
Andrei Neustroev <andreineustroev02@gmail.com>
2022-03-03 03:56:29 +0900

    hotfix

--- src/frontend/api/index.ts
+++ src/frontend/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const baseURL = "https://next-pikabu-andreineu.vercel.app/api";
-const baseURL = "https://pikabu-nextjs.herokuapp/api";
+const baseURL = "https://pikabu-nextjs.herokuapp.com/api";
 // const baseURL = "https://localhost:3000";
 
 export const api = axios.create({ baseURL: baseURL });
